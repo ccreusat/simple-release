3be91528803e7d9b4b6094f7f9188e30ad583376
Clement Creusat <clement.creusat@gmail.com>
2023-12-24 17:35:03 +0100

    feat: add get latest tag fn

--- src/get-latest-tag.js
+++ src/get-latest-tag.js
@@ -2,17 +2,21 @@ import { simpleGit } from "simple-git";
 
 const git = simpleGit();
 
-git.tags((err, tags) => {
-  if (err) {
-    console.error("Erreur lors de la récupération des tags:", err);
-    return;
-  }
+export function getLastTag() {
+  git.tags((err, tags) => {
+    if (err) {
+      console.error("Something wrong happened", err);
+      return;
+    }
 
-  const lastTag = tags.latest;
+    const lastTag = tags.latest;
 
-  if (!lastTag) {
-    console.log("No tag found");
-    return;
-  }
-  console.log("Dernier tag créé:", lastTag);
-});
+    if (!lastTag) {
+      console.log("No tag found");
+      return;
+    }
+    console.log("Last tag is:", lastTag);
+  });
+}
+
+getLastTag();
