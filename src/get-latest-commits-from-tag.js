7885634334d1c74169c030874c7daeb3684399c3
Clement Creusat <clement.creusat@gmail.com>
2023-12-25 11:05:52 +0100

    fix: add tag fn in commit file

--- src/get-latest-commits-from-tag.js
+++ src/get-latest-commits-from-tag.js
@@ -1,15 +1,27 @@
 import { simpleGit } from "simple-git";
-import { getLastTag } from "./get-latest-tag.js";
 
 const git = simpleGit();
 
-const lastTag = getLastTag();
-
-git.log({ from: lastTag }, (err, commits) => {
-  if (err) {
-    console.error("Something wrong happened:", err);
+git.tags((error, tags) => {
+  if (error) {
+    console.error("Something wrong happened", error);
     return;
   }
 
-  console.log("Commits since last tag:", commits.all);
+  const lastTag = tags.latest;
+
+  if (lastTag) {
+    console.log("Last tag is:", lastTag);
+  } else {
+    console.log("No tag found");
+  }
+
+  git.log({ from: lastTag }, (err, commits) => {
+    if (err) {
+      console.error("Something wrong happened:", err);
+      return;
+    }
+
+    console.log("Commits since last tag:", commits.all);
+  });
 });
