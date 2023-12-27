a17baadde7282ab1d59564baa9ce662cc31cb26c
Clement Creusat <clement.creusat@gmail.com>
2023-12-27 19:33:01 +0100

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import chalk from "chalk";
 import { getConfig } from "./find-cosmiconfig.js";
 import { getCurrentBranch } from "./get-current-branch.js";
 import { getGitStatus } from "./get-status.js";
+import { execa } from "execa";
 
 const config = await getConfig();
 
@@ -29,6 +30,22 @@ function getVersionPreid() {
   return preid;
 }
 
+async function prerelease() {
+  try {
+    const result = await execa("npm", [
+      "version",
+      "prerelease",
+      "--preid",
+      "alpha",
+    ]);
+
+    console.log({ result });
+  } catch (error) {
+    console.log(error);
+    // console.error(chalk.red(`${error}`));
+  }
+}
+
 async function run() {
   try {
     const versionPrefix = getVersionPreid();
@@ -39,13 +56,12 @@ async function run() {
       process.exit(1);
     }
     // Exécutez la commande npm version prerelease avec le préfixe de version
-    const { stdout } = await execa("npm", [
+    const result = await execa("npm", [
       "version",
       "prerelease",
       "--preid",
       versionPrefix,
     ]);
-    console.log({ stdout });
     console.log(chalk.green(stdout));
   } catch (error) {
     console.error(chalk.red(`Unable to version "${currentBranch}"`));
@@ -53,4 +69,4 @@ async function run() {
   }
 }
 
-run();
+prerelease();
